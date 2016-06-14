7ce80cca2f61c953cabdf56211af05a9b84c2f59
Tsarev Nikita <nikts53@gmail.com>
2016-06-14 20:34:24 +0300

    Groups now deny non-members

--- pysistem/groups/views.py
+++ pysistem/groups/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from pysistem import app, db
 from flask import render_template, session, g, flash, redirect, url_for, request, Blueprint, Response
-from pysistem.groups.decorators import yield_group
+from pysistem.groups.decorators import yield_group, requires_group_membership
 from pysistem.users.decorators import requires_admin
 
 mod = Blueprint('groups', __name__, url_prefix='/group')
@@ -14,6 +14,7 @@ def users(id, group):
 
 @mod.route('/<int:id>/contests')
 @yield_group()
+@requires_group_membership()
 def contests(id, group):
     raw = render_template('contests/rawlist.html', contests=group.contests)
     return render_template('groups/contests.html', group=group, rawlist=raw)
