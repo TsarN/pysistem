b54c66d51e80f8e35d47aa4d135781b9eaa26545
Tsarev Nikita <nikts53@gmail.com>
2016-06-18 21:12:29 +0300

    Added group create and group rename

--- pysistem/groups/views.py
+++ pysistem/groups/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from pysistem import app, db
+from pysistem import app, db, redirect_url
 from flask import render_template, session, g, flash, redirect, url_for, request, Blueprint, Response
 from pysistem.groups.decorators import yield_group, requires_group_membership
 from pysistem.users.decorators import requires_admin
 from pysistem.lessons.model import Lesson
+from pysistem.groups.model import Group
+from flask_babel import gettext
 
 mod = Blueprint('groups', __name__, url_prefix='/group')
 
@@ -25,3 +27,28 @@ def contests(id, group):
 @requires_admin(group="group")
 def lessons(id, group):
     return render_template('groups/lessons.html', group=group)
+
+@mod.route('/create', methods=['POST'])
+@requires_admin()
+def create():
+    name = request.form.get('name', '')
+    if len(name) < 1:
+        flash('::danger ' + gettext('groups.edit.emptyname'))
+    else:
+        db.session.add(Group(name=name))
+        db.session.commit()
+        flash(gettext('groups.create.success'))
+    return redirect(redirect_url())
+
+@mod.route('/rename/<int:id>', methods=['POST'])
+@yield_group()
+@requires_admin(group="group")
+def rename(id, group):
+    name = request.form.get('name', '')
+    if len(name) < 1:
+        flash('::danger ' + gettext('groups.edit.emptyname'))
+    else:
+        group.name = name
+        db.session.commit()
+        flash(gettext('groups.rename.success'))
+    return redirect(redirect_url())
