bedd9dfbf9a4548e185261da472a5ab34de4e014
Tsarev Nikita <nikts53@gmail.com>
2016-06-22 15:41:43 +0300

    Dropped support for vm

--- vm/usr/local/lib/pysistem/fixdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Factory resetting database..." &&
-"/usr/local/lib/pysistem/generateschema.sh" | mysql -u root -p &&
-echo "Clearing storage" &&
-rm -rf "$HOME/env/pysistem/pysistem/storage"
--- vm/usr/local/lib/pysistem/generateschema.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. "$HOME/env/pysistem/bin/activate"
-pushd "$HOME/env/pysistem" > /dev/null 2>&1
-h=`echo 'from pysistem.users.model import User; print(User.signpasswd("admin", "gazorpazorpfield"))' | python`
-cat "/usr/local/lib/pysistem/defaultschema.sql" | sed "s/__SIGNED_ADMIN_PASSWORD__/$h/g" 
-popd > /dev/null 2>&1
